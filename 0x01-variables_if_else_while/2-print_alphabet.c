9eb1bcd7ffed198227f3b7fb90aae15d7872baef
omar <omarali2599@gmail.com>
2023-02-18 12:22:56 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - potato
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar(lc);
+	return (0);
+}
