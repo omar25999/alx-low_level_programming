8111748130e7f3d9c6d1eaaf62dc4ad7c0782679
omar <omarali2599@gmail.com>
2023-02-18 12:58:17 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - potato
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+			putchar(lc);
+	}
+
+	putchar('\n');
+	return (0);
+}
