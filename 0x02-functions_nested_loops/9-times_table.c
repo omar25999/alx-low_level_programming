e8995c9e9b2e550fdce797ed0710acd0de5fc18c
omar <omarali2599@gmail.com>
2023-02-21 14:51:06 -0800

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,80 @@
+#include "main.h"
+/**
+ * times_table - function that multiply table by 9
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int l;
+	int m;
+	int p;
+	int x;
+	int y;
+	int z;
+	int w;
+	int t;
+	int r;
+	int a;
+
+	for (l = 0; l <= 10; l++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (m = 0; m <= 10; m++)
+	{
+		_putchar(m);
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (p = 0; p <= 10; p++)
+	{
+		_putchar(p + '2');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (x = 0; x <= 10; x++)
+	{
+		_putchar(x + '3');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (y = 0; y <= 10; y++)
+	{
+		_putchar(y + '4');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (z = 0; z <= 10; z++)
+	{
+		_putchar(z + '5');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (w = 0; w <= 10; w++)
+	{
+		_putchar(w + '6');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (t = 0; t <= 10; t++)
+	{
+		_putchar(t + '7');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (r = 0; r <= 10; r++)
+	{
+		_putchar(r + '8');
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (a = 0; a <= 10; a++)
+	{
+		_putchar(a + '9');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+
