f5665426739e93c56ddaa7da3d87f1722911f460
omar <omarali2599@gmail.com>
2023-02-23 05:41:16 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers followed by new line
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int f;
+
+	for (f = '0'; f <='9'; f++)
+		_putchar(f);
+}
