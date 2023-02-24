22076179b65da08ab1d07f312710f9e3cbbbc933
omar <omarali2599@gmail.com>
2023-02-24 08:38:37 -0800

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -20,19 +24,14 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else if (x % 15 == 0)
-		{
-			printf("FizzBuzz");
-		}
 		else
 		{
 			printf("%d", x);
 		}
-		if (x != 0)
+		if (x != 100)
 		{
 			printf(" ");
 		}
-
 	}
 	printf("\n");
 	return (0);
