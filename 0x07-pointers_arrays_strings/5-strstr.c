e0e37c3f6886b1e550234a9f3a49791398cfb22d
omar <omarali2599@gmail.com>
2023-03-07 03:03:30 -0800

    7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: depends
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagsums - function that prints the sum of the two diagonals
+ * @a: pointer to arrays
+ * @size: size of array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 0; x++)
+	{
+		for (y = 0; y < 0; y++)
+		{
+			_putchar(a[x][y]);
+			_putchar('\n');
+		}
+	}
+}
+
